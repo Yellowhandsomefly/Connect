4a483bba9f8934dcac7189abbba5daa62bac056c
Yellowhandsomefly <I279KenEmily22@gmail.com>
2016-12-26 21:58:26 +0800

    Design a  FB login activity

---
+++ app/src/main/java/com/ehappy/connect/loginActivity.java
@@ -0,0 +1,25 @@
+package com.ehappy.connect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
+
+public class loginActivity extends AppCompatActivity {
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+    }
+}
