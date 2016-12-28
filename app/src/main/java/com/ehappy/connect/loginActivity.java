b8297f3cfaf6d3a696fd8624696e73cf3f8d5a39
Yellowhandsomefly <I279KenEmily22@gmail.com>
2016-12-28 18:18:38 +0800

    handle the condition when login

--- app/src/main/java/com/ehappy/connect/loginActivity.java
+++ app/src/main/java/com/ehappy/connect/loginActivity.java
@@ -1,12 +1,19 @@
 package com.ehappy.connect;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class loginActivity extends AppCompatActivity {
     private LoginButton loginButton;
@@ -20,6 +27,34 @@ public class loginActivity extends AppCompatActivity {
 
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton)findViewById(R.id.login_button);
+        callbackManager = CallbackManager.Factory.create();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email", "user_status"));
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        Intent intent = new Intent(loginActivity.this, MainActivity.class);
+                        intent.putExtra("fbUserId", loginResult.getAccessToken().getUserId());
+                        startActivity(intent);
+                    }
 
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        Toast.makeText(getApplication(), "Login attempt canceled.", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        Toast.makeText(getApplication(), "Login attempt failed.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
