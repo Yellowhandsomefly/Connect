ab3863fd17b3017ded654bc53ddbdf5d6b4bc741
Yellowhandsomefly <I279KenEmily22@gmail.com>
2016-12-30 15:51:36 +0800

    combine fb login and firebase

--- app/src/main/java/com/ehappy/connect/loginActivity.java
+++ app/src/main/java/com/ehappy/connect/loginActivity.java
@@ -1,44 +1,56 @@
 package com.ehappy.connect;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
+import java.util.concurrent.Executor;
 
 public class loginActivity extends AppCompatActivity {
     private LoginButton loginButton;
     private CallbackManager callbackManager;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private String userId = " ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_login);
-
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton)findViewById(R.id.login_button);
-        callbackManager = CallbackManager.Factory.create();
-        loginButton = (LoginButton) findViewById(R.id.login_button);
+
         loginButton.setReadPermissions(Arrays.asList("email", "user_status"));
         loginButton.registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
                         // App code
-                        Intent intent = new Intent(loginActivity.this, MainActivity.class);
-                        intent.putExtra("fbUserId", loginResult.getAccessToken().getUserId());
-                        startActivity(intent);
-                    }
+                       // userId = loginResult.getAccessToken().getUserId();
+                        handleFacebookAccessToken(loginResult.getAccessToken());                    }
 
                     @Override
                     public void onCancel() {
@@ -53,8 +65,55 @@ public class loginActivity extends AppCompatActivity {
                     }
                 });
 
+            firebaseAuth = FirebaseAuth.getInstance();
+            firebaseAuthListener = new FirebaseAuth.AuthStateListener(){
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if(user != null){
+                        goToMain();
+                    }
+                }
+            };
     }
+
+
+
+    private void handleFacebookAccessToken(AccessToken accessToken){
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener( this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(getApplication(), "firebase_error_login.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void goToMain(){
+        Intent intent = new Intent(loginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("fbUserId", userId);
+        startActivity(intent);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (firebaseAuthListener != null) {
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+        }
     }
 }
